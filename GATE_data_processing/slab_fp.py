9290bf37232995f91842cd717b181f8d3a85ad1b
Edward <edward.smith-4@postgrad.manchester.ac.uk>
2019-07-23 00:04:33 +0100

    file paths for slab data

---
+++ GATE_data_processing/slab_fp.py
@@ -0,0 +1,34 @@
+# Filepath Dict
+
+fp_folder = 'C:\\Users\\mdefses3\\CODE\\Python\\LET Addition Data Store\\EAKS CW SOBP Sim\\'
+
+fp_orig = fp_folder + '20190608_Orig_Sim\\output\\'
+
+fp_SLInvest_1 = fp_folder + '20190628_SL_Investigation\\1\\output\\'
+fp_SLInvest_2 = fp_folder + '20190628_SL_Investigation\\2\\output\\'
+fp_SLInvest_3 = fp_folder + '20190628_SL_Investigation\\3\\output\\'
+fp_SLInvest_4 = fp_folder + '20190628_SL_Investigation\\4\\output\\'
+fp_SLInvest_5 = fp_folder + '20190628_SL_Investigation\\5\\output\\'
+fp_SLInvest_6 = fp_folder + '20190628_SL_Investigation\\6\\output\\'
+fp_SLInvest_7 = fp_folder + '20190628_SL_Investigation\\7\\output\\'
+fp_SLInvest_8 = fp_folder + '20190628_SL_Investigation\\8\\output\\'
+fp_SLInvest_9 = fp_folder + '20190628_SL_Investigation\\9\\output\\'
+
+fp_LETAdd_ball = fp_folder + '20190629_Beam_Addition_Issue\\all_beams\\output\\'
+fp_LETAdd_b01 = fp_folder + '20190629_Beam_Addition_Issue\\beam_1\\output\\'
+fp_LETAdd_b02 = fp_folder + '20190629_Beam_Addition_Issue\\beam_2\\output\\'
+fp_LETAdd_b03 = fp_folder + '20190629_Beam_Addition_Issue\\beam_3\\output\\'
+fp_LETAdd_b04 = fp_folder + '20190629_Beam_Addition_Issue\\beam_4\\output\\'
+fp_LETAdd_b05 = fp_folder + '20190629_Beam_Addition_Issue\\beam_5\\output\\'
+fp_LETAdd_b06 = fp_folder + '20190629_Beam_Addition_Issue\\beam_6\\output\\'
+fp_LETAdd_b07 = fp_folder + '20190629_Beam_Addition_Issue\\beam_7\\output\\'
+fp_LETAdd_b08 = fp_folder + '20190629_Beam_Addition_Issue\\beam_8\\output\\'
+fp_LETAdd_b09 = fp_folder + '20190629_Beam_Addition_Issue\\beam_9\\output\\'
+fp_LETAdd_b10 = fp_folder + '20190629_Beam_Addition_Issue\\beam_10\\output\\'
+
+fp_hittype_ran = fp_folder + '20190712_hit_type\\hit_type_ran\\output\\'
+
+fp_LET2Med_Bone = fp_folder + '20190703_LET_to_Medium\\water_and_bone\\let_to_medium\\output\\'
+fp_LET2Water_Bone = fp_folder + '20190703_LET_to_Medium\\water_and_bone\\let_to_water\\output\\'
+
+fp_LET2Med_Water = fp_folder + '20190703_LET_to_Medium\\watertank\\let_to_medium\\output\\'
